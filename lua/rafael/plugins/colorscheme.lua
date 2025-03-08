fd6634bc79bc210a58b78a4703bb66df92a117cd
Rafael Morais <rafahgmorais@gmail.com>
2025-03-08 14:18:25 -0300

    feat(colorscheme): define variação do tema para ser utilizado

--- lua/rafael/plugins/colorscheme.lua
+++ lua/rafael/plugins/colorscheme.lua
@@ -3,5 +3,8 @@ return {
     priority = 1000,
     config = function()
         vim.cmd("colorscheme tokyonight")
+        require("tokyonight").setup({
+            style = "moon"
+        })
     end
 }
