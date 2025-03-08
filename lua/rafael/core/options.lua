ae7773fe88217cd2ecadbaa938ecca6a01fa8d19
Rafael Morais <rafahgmorais@gmail.com>
2025-03-08 13:52:33 -0300

    feat(opções): define configurações básicas do neovim

---
+++ lua/rafael/core/options.lua
@@ -0,0 +1,29 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs e identação
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.cursorline = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
