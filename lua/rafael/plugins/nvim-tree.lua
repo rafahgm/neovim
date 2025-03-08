680de53aa935254efc2a1eb14b572e583a0e90ac
Rafael Morais <rafahgmorais@gmail.com>
2025-03-08 14:19:06 -0300

    feat(nvim-tree): add nvim-tree

---
+++ lua/rafael/plugins/nvim-tree.lua
@@ -0,0 +1,47 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+        local nvimtree = require("nvim-tree")
+
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        nvimtree.setup({
+            view = {
+                width = 35,
+                relativenumber = true
+            },
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                },
+                icons = {
+                    glyphs = {
+                        folder = {
+                            arrow_closed = "",
+                            arrow_open = "",
+                        }
+                    }
+                }
+            },
+            actions = {
+                open_file = {
+                    window_picker = {
+                        enable = false,
+                    }
+                }
+            },
+            git = {
+                ignore = false,
+            }
+        })
+
+        local keymap = vim.keymap
+
+        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Habilita/desabilita explorador de arquivos" })
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Habilita/desabilita o explorar de arquivos no arquivo atual" })
+        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Colapsa o explorador de arquivos" })
+        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Recarrega o explorador de arquivos" })
+    end
+}
